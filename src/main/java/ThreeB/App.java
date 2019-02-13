02e727384ec879bc0803977ecceb0752e51ef20b
jaco <jpjdebeer@gmail.com>
2019-02-13 21:42:31 +0200

    Initial commit

---
+++ src/main/java/ThreeB/App.java
@@ -0,0 +1,14 @@
+package ThreeB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("ADP3 Assignment1");
+    }
+}
